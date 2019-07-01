d8ddd7c1db6063cf8c525bc798e17cbd7af7f7cf
matiiss-z <matiissdz@gmail.com>
2019-07-01 22:11:12 +0300

    Add environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,29 @@
 const greeting: string = 'heelo';
 
+const dotenv = require('dotenv');
+dotenv.config();
 const numbers: number[] = [1,2,3];
+
 
 import express, {Application, Request, Response, NextFunction} from 'express';
 import bodyParser from 'body-parser';
 const MongoClient = require('mongodb').MongoClient;
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 const twit = require('twit');
 
 const users = [];
 
 // const twitter = new twit({
-//   consumer_key: '',
-//   consumer_secret: ' ... ',
-//   access_token: ' ... ',
-//   access_token_secret: ' ... '
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 // })
 
-//Next step - push this to heroku check how to deploy correctly then deploy frontend to test if this setup will work 
-//on simple setup
+console.log(process.env.TWITTER_CONSUMER_KEY);
+console.log(process.env.TWITTER_ACCESS_TOKEN_SECRET);
+
 
 const cors = require('cors');
 
@@ -39,4 +43,4 @@ app.get('/tweet', (req: Request, res: Response) => {
   res.send(tweets);
 });
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on PORT ${port}`));
